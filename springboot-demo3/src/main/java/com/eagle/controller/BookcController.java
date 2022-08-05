29f6a7f9685634b73648a596d3c9da1d1a2f1426
franksong777 <franksong666@yeah.net>
2022-08-05 18:42:17 -0400

    add logging config in pom.xml

--- springboot-demo3/src/main/java/com/eagle/controller/BookcController.java
+++ springboot-demo3/src/main/java/com/eagle/controller/BookcController.java
@@ -1,17 +1,24 @@
 package com.eagle.controller;
 
 import com.eagle.controller.utils.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j              // for logging  after dependency lombok in pom.xml , straight use log.info(); below
 @RestController
 @RequestMapping("/books")
 public class BookcController {
 
     @GetMapping("/{number}")
     public int getById(@PathVariable Integer number){
+
+        log.debug("debug...");
+        log.info("info...");
+        log.warn("warn...");
+        log.error("error...");
+
 
         return number==Result.correct?Result.correct:Result.incorrect;
     }
